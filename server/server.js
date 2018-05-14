97e648e5fda46bfcc9331413163194fd4774b4fb
sam80719 <sam80719@gmail.com>
2018-05-14 21:55:59 +0800

    add style to chat app

--- server/server.js
+++ server/server.js
@@ -1,9 +1,99 @@
+// const path = require('path');
+// const http = require('http');
+// const express = require('express');
+// const socketIO = require('socket.io');
+
+// const { generateMessage, generateLocationMessage } = require('./utils/message');
+// const publicPath = path.join(__dirname, '../public');
+// const port = process.env.PORT || 3000;
+// var app = express();
+// var server = http.createServer(app);
+// var io = socketIO(server);
+
+// app.use(express.static(publicPath));
+
+// io.on('connection', (socket) => {
+//     console.log('New user connected');
+
+//     // socket.emit('newEmail', {
+//     //     from: 'sam@example.com',
+//     //     text: 'Hey. What is going on.',
+//     //     createAt: 123
+//     // });
+//     // socket.emit('newMessage', {
+//     // 	from:'Sam',
+//     // 	text:'See you then',
+//     // 	createAt:0973169236
+//     // });
+
+//     // socket.on('createEmail', (newEmail) => {
+//     //     console.log('createEmail', newEmail);
+//     // });
+
+
+
+//     socket.emit('newMessage', generateMessage('admin', 'welcome to the chat app'));
+//     socket.broadcast.emit('newMessage', generateMessage('admin', 'new user joined'));
+
+
+//     //socket.emit from admin text welcome to the chat app
+//     // socket.broadcast.emit from admin text new user joined
+//     // emit觸發事件
+//     // socket.emit('newMessage',{
+//     // 	from: 'admin',
+//     // 	text: 'welcome to the chat app',
+//     // 	createAt: new Date().getTime() 
+//     // });
+//     // //broadcast 監聽與廣播事件
+//     // socket.broadcast.emit('newMessage', {
+//     // 		from: 'admin',
+//     // 		text: 'new user joined',
+//     // 		createAt: new Date().getTime()    	
+//     // });
+
+
+
+
+//     socket.on('createMessage', (message, callback) => {
+//         console.log('createMessage', message);
+
+//         io.emit('newMessage', generateMessage(message.from, message.text));
+//         callback('this is from server.');
+//         // io.emit('newMessage',{
+//         // 	from: message.from,
+//         // 	text: messa ge.text,
+//         // 	createAt: new Date().getTime()
+//         // });
+//         // socket.broadcast.emit('newMessage', {
+//         // 	from: message,
+//         // 	text: message.text,
+//         // 	createAt: new Date().getTime()
+
+//         // });
+
+
+//     });
+
+
+//     socket.on('createLocationMessage', (coords) => {
+//         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+//     });
+
+
+//     socket.on('disconnect', () => {
+//         console.log('User was disconnected');
+//     });
+// });
+
+// server.listen(port, () => {
+//     console.log(`Server is up on ${port}`);
+// });
 const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const { generateMessage, generateLocationMessage } = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -13,78 +103,28 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+  console.log('New user connected');
 
-    // socket.emit('newEmail', {
-    //     from: 'sam@example.com',
-    //     text: 'Hey. What is going on.',
-    //     createAt: 123
-    // });
-    // socket.emit('newMessage', {
-    // 	from:'Sam',
-    // 	text:'See you then',
-    // 	createAt:0973169236
-    // });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to Sam\'s chat app'));
 
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
-    socket.emit('newMessage', generateMessage('admin', 'welcome to the chat app'));
-    socket.broadcast.emit('newMessage', generateMessage('admin', 'new user joined'));
-
-
-    //socket.emit from admin text welcome to the chat app
-    // socket.broadcast.emit from admin text new user joined
-    // emit觸發事件
-    // socket.emit('newMessage',{
-    // 	from: 'admin',
-    // 	text: 'welcome to the chat app',
-    // 	createAt: new Date().getTime() 
-    // });
-    // //broadcast 監聽與廣播事件
-    // socket.broadcast.emit('newMessage', {
-    // 		from: 'admin',
-    // 		text: 'new user joined',
-    // 		createAt: new Date().getTime()    	
-    // });
-
-
-
-
-    socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
-
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('this is from server.');
-        // io.emit('newMessage',{
-        // 	from: message.from,
-        // 	text: messa ge.text,
-        // 	createAt: new Date().getTime()
-        // });
-        // socket.broadcast.emit('newMessage', {
-        // 	from: message,
-        // 	text: message.text,
-        // 	createAt: new Date().getTime()
-
-        // });
-
-
-    });
-
-
-    socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-    });
-
-
-    socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
 server.listen(port, () => {
-    console.log(`Server is up on ${port}`);
+  console.log(`Server is up on ${port}`);
 });
+
